f395365c73cc117958f7113e66d64e4eb2bec94b
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-13 00:46:12 +0100

    Initial Commit

---
+++ NavigationSimpleButDeprecated/NavigationSimpleButDeprecatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSimpleButDeprecatedApp.swift
+//  NavigationSimpleButDeprecated
+//
+//  Created by Zaid Neurothrone on 2022-12-13.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSimpleButDeprecatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
